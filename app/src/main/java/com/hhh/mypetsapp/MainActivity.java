f17ca6e1f4c87efc0bbb79e05e7e60bfa7ac8282
Aily-Hhh <aily.hhh13@gmail.com>
2022-06-22 17:27:21 +0300

    update
    Firebase UI + locate + main activity

--- app/src/main/java/com/hhh/mypetsapp/MainActivity.java
+++ app/src/main/java/com/hhh/mypetsapp/MainActivity.java
@@ -1,194 +1,42 @@
 package com.hhh.mypetsapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
-
-import android.app.Activity;
-import android.content.DialogInterface;
+import android.os.Bundle;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button signInButton;
-    Button registrationButton;
-    FirebaseAuth firebaseAuth;
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference users;
-    ConstraintLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        signInButton = findViewById(R.id.signInButton);
-        registrationButton = findViewById(R.id.registrationButton);
-        mainLayout = findViewById(R.id.mainLayout);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        users = firebaseDatabase.getReference("users");
-
-        registrationButton.setOnClickListener(new View.OnClickListener() {
+        Button signOutButton = (Button) findViewById(R.id.signOutButton);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showRegistrationWindow();
-            }
-        });
-
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showSignInWindow();
-            }
-        });
-    }
-
-    public void showRegistrationWindow() {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("Регистрация");
-        dialog.setMessage("Введите данные для регистрации");
-
-        LayoutInflater layoutInflater = LayoutInflater.from(this);
-        View registrationWindow = layoutInflater.inflate(R.layout.activity_register, null);
-        dialog.setView(registrationWindow);
-
-        EditText email = registrationWindow.findViewById(R.id.emailEditText);
-        EditText userName = registrationWindow.findViewById(R.id.userNameEditText);
-        EditText phone = registrationWindow.findViewById(R.id.phoneEditText);
-        EditText pass = registrationWindow.findViewById(R.id.passEditText);
-
-        dialog.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        });
-
-        dialog.setPositiveButton("Ок", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if (TextUtils.isEmpty(email.getText().toString())) {
-                    Snackbar.make(mainLayout, "Введите вашу почту", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(userName.getText().toString())) {
-                    Snackbar.make(mainLayout, "Введите ваше имя", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(phone.getText().toString())) {
-                    Snackbar.make(mainLayout, "Введите ваш номер телефона", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(pass.getText().toString())) {
-                    Snackbar.make(mainLayout, "Введите пароль", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-                if (pass.getText().toString().length() < 6) {
-                    Snackbar.make(mainLayout, "Введен пароль менее 6 символов", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-
-                //Registration
-                firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
-                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
-                            public void onSuccess(AuthResult authResult) {
-                                User user = new User();
-                                user.setEmail(email.getText().toString());
-                                user.setUserName(userName.getText().toString());
-                                user.setPhone(phone.getText().toString());
-                                user.setPassword(pass.getText().toString());
-
-                                users.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                        .setValue(user)
-                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                            @Override
-                                            public void onSuccess(Void unused) {
-                                                Snackbar.make(mainLayout, "Успешно",
-                                                        Snackbar.LENGTH_SHORT).show();
-                                            }
-                                        });
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Snackbar.make(mainLayout, "" + e.getMessage(),
-                                Snackbar.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-        dialog.show();
-    }
-
-    public void showSignInWindow() {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("Вход");
-        dialog.setMessage("Введите данные для входа");
-
-        LayoutInflater layoutInflater = LayoutInflater.from(this);
-        View signInWindow = layoutInflater.inflate(R.layout.sign_in_activity, null);
-        dialog.setView(signInWindow);
-
-        EditText email = signInWindow.findViewById(R.id.emailEditText);
-        EditText pass = signInWindow.findViewById(R.id.passEditText);
-
-        dialog.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        });
-
-        dialog.setPositiveButton("Ок", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if (TextUtils.isEmpty(email.getText().toString())) {
-                    Snackbar.make(mainLayout, "Введите корректную почту", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-                if (TextUtils.isEmpty(pass.getText().toString())) {
-                    Snackbar.make(mainLayout, "Введите корректный пароль", Snackbar.LENGTH_SHORT).show();
-                    return;
-                }
-
-                firebaseAuth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
-                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                            @Override
-                            public void onSuccess(AuthResult authResult) {
-                                startActivity(new Intent(MainActivity.this, Second.class));
-                                finish();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Snackbar.make(mainLayout, "" + e.getMessage(),
-                                        Snackbar.LENGTH_SHORT).show();
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(MainActivity.this, R.string.userSignedOut, Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+                                startActivity(intent);
                             }
                         });
             }
         });
-
-        dialog.show();
     }
 }
